9f509914527fb10f4a98233a551d866c1787a8c5
KimMinJae <smallkdb@gmail.com>
2022-03-13 23:13:29 +0900

    learn CSSModule

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import SassComponent from "./SassComponent";
-
+import StyledComponent from "./StyledComponent";
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
